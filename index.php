c132ce9931fe3200c44e357c40eed8f2f3d12759
Andrey Volynkin <95611276+AndreyVA1155@users.noreply.github.com>
2022-02-14 19:31:39 +0200

    //простой обход папки без указания вложенности

--- index.php
+++ index.php
@@ -1,4 +1,27 @@
 <?php
-$dir = "c:\\windows\\system32";
+error_reporting(E_ALL);
+ini_set('display_errors', 'on');
+
+
+$dir = "c:\\MinGW";
 //простой обход папки без использования ООП
+getFiles($dir);
+function getFiles($dir) {
+    $files  = array_diff(scandir($dir), ['..', '.']);
+    $result = [];
+
+    foreach ($files as $file) {
+        $path = $dir . '/' . $file;
+        if (is_dir($path)) {
+            $result = array_merge($result, getFiles($path));
+        } else {
+            $result[] = $path;
+        }
+    }
+    return $result;
+}
+$array = getFiles($dir);
+echo '<pre>';
+var_dump($array);
+echo '</pre>';
 ?>
